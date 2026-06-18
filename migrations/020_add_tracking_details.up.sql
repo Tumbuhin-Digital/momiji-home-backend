38a3ea9e2fed18afd03e35df565e3870fd5deccd
Yusuf Rachmad W <walkidain@gmail.com>
2026-06-18 15:26:56 +0700

    fix(migration): change table name

--- migrations/020_add_tracking_details.up.sql
+++ migrations/020_add_tracking_details.up.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER TABLE order_items
+ALTER TABLE order_line_items
   ADD COLUMN IF NOT EXISTS tracking_company VARCHAR(128),
   ADD COLUMN IF NOT EXISTS tracking_last_event VARCHAR(255);
 
